81af577fc2119826df7ec966c66a23f91e4d78dc
janikbenzin <janikbenzin@mailbox.org>
2025-09-29 17:10:55 +0200

    Update install

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ source .venv/bin/activate
 pip install -r requirements.txt
 pip install xes-yaml-pm4py-extension/pm4py-2.7.11.11-py3-none-any.whl
 gem install cpee-logging-xes-yaml
+mkdir sim_data
+mkdir run_data
 cd sim_data/
 curl --output runs_all_iot_systems.tgz "https://zenodo.org/api/records/16310813/files-archive"
 tar -xvzf runs_all_iot_systems.tgz
